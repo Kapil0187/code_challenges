332071828ce7838c025e716adf33d93ec8b8ee3a
Kapil <kapilbarsker2003@gmail.com>
2025-02-06 01:28:45 +0530

    Setup React and Tailwind CSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+     <div>
+      This is the App component.
+     </div>
+    </div>
+  )
+}
+
+export default App
